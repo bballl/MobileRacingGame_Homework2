76e7773474da9b8f92ac49fae04d9ffca62c0420
TikhonovGleb <bball2011@yandex.ru>
2021-12-31 00:06:23 +0300

    Homework

---
+++ MobileTestGame/Assets/Scripts/ProfilePlayer.cs
@@ -0,0 +1,22 @@
+﻿using Profile;
+using Tools;
+
+public class ProfilePlayer
+{
+    public ProfilePlayer(float speedCar, UnityAdsTools unityAdsTools)
+    {
+        CurrentState = new SubscriptionProperty<GameState>();
+        CurrentCar = new Car(speedCar);
+        AnalyticTools = new UnityAnalyticTools();
+        AdsShower = unityAdsTools;
+    }
+
+    public SubscriptionProperty<GameState> CurrentState { get; }
+
+    public Car CurrentCar { get; }
+
+    public IAnalyticTools AnalyticTools { get; }
+
+    public IAdsShower AdsShower { get; }
+}
+
---
+++ MobileTestGame/Assets/Scripts/Root.cs
@@ -0,0 +1,25 @@
+﻿using Profile;
+using UnityEngine;
+
+public class Root : MonoBehaviour
+{
+    [SerializeField] 
+    private Transform _placeForUi;
+
+    [SerializeField]
+    private UnityAdsTools _unityAdsTools;
+
+    private MainController _mainController;
+
+    private void Awake()
+    {
+        var profilePlayer = new ProfilePlayer(15f, _unityAdsTools);
+        profilePlayer.CurrentState.Value = GameState.Start;
+        _mainController = new MainController(_placeForUi, profilePlayer);
+    }
+
+    protected void OnDestroy()
+    {
+        _mainController?.Dispose();
+    }
+}
